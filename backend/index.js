ee7e408f5d73a7359fad20d0da0f74fb67442c0b
farhanaWdev <wfarhanaislam@gmail.com>
2026-09-06 22:35:51 +0600

    add router

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const authRouter = require('./routes/authRouter')
+
+
+
+
+// main route
+app.use('/api/v1/auth' ,authRouter)
+
+
+
+const port = process.env.PORT || 5000
+app.listen(port,()=>{
+    console.log(`Server is running on port: ${port}`)
+})
