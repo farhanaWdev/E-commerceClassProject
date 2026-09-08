2e1512c44e03729f5e47ec3f2c99bccbeb8b1e62
farhanaWdev <wfarhanaislam@gmail.com>
2026-09-08 20:33:26 +0600

    fix: correct sender email address in verification email function

--- backend/utils/emailSender.js
+++ backend/utils/emailSender.js
@@ -4,7 +4,6 @@ const { getMaxListeners } = require("../models/userSchema");
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
   services: "gmail",
-
   secure: false, 
   auth: {
     user:' process.env.NODEMAILER_EMAIL',
@@ -15,7 +14,7 @@ const transporter = nodemailer.createTransport({
 async function verificationEmail(email,token){
     try {
       const info = await transporter.sendMail({
-          from: 'farha268na@gmail.com', 
+          from: 'farha168na@gmail.com', 
           to: email, 
           subject: "Please Verify Your Email", 
           html: '<b>Please verify your email to unlock full access . Kindly Click Here to Procced <a href="http://localhost:5173/verify/${token}">There you go</a> </b>',
