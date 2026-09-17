b20c05320fdf927366c1018f85731f803ab41388
farhanaWdev <wfarhanaislam@gmail.com>
2026-09-18 01:48:04 +0600

    fix: uncomment allusersControler function and export in adminController

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,16 +1,16 @@
-// const AllUser = require('../models/userSchema')
+const AllUser = require('../models/userSchema')
 
-// let allusersControler  =async (req,res)=>{
-//     let users = await AllUser.find({}).select('-password')
-//     res.status(200).json({
-//         success:true,
-//         message:`${users.length} users  found`,
-//         data:users
-//     })
-//     console.log("hello")
-// }
+let allusersControler  =async (req,res)=>{
+    let users = await AllUser.find({}).select('-password')
+    res.status(200).json({
+        success:true,
+        message:`${users.length} users  found`,
+        data:users
+    })
+    console.log("hello")
+}
 
-// module.exports = {allusersControler}
+module.exports = {allusersControler}
 
 
 
