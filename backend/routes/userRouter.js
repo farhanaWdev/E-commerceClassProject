71fede707274b26271043dcbe6fe1dd24c527018
farhanaWdev <wfarhanaislam@gmail.com>
2026-09-19 02:24:07 +0600

    fix: routes update

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,12 +1,12 @@
-// const express = require('express');
-// const { userController, createCategory, getAllCategory } = require('../controllers/userController');
-// const _ = express.Router();
+const express = require('express');
+const { userController, createCategory, getAllCategory } = require('../controllers/userController');
+const _ = express.Router();
 
-// // All Controllers
-// _.get('/product', userController);
-// _.post('/create/category', createCategory);
-// _.get('/all/category', getAllCategory);
+// All Controllers
+_.get('/product', userController);
+_.post('/create/category', createCategory);
+_.get('/all/category', getAllCategory);
 
 
 
-// module.exports = _;
+module.exports = _;
