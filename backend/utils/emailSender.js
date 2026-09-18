06656cd3eaaa448d8610a4bce7f0eec64239ce0e
farhanaWdev <wfarhanaislam@gmail.com>
2026-09-19 02:26:48 +0600

    update email

--- backend/utils/emailSender.js
+++ backend/utils/emailSender.js
@@ -1,7 +1,5 @@
 const nodemailer = require("nodemailer");
-// const { getMaxListeners } = require("../models/userSchema");
 
-// Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
   service: "gmail",
   port:587,
