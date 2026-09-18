ea4b52d77cde7bc5584ff7cdbe5aef8faf14c33e
farhanaWdev <wfarhanaislam@gmail.com>
2026-09-19 02:23:05 +0600

    refactor:  implement category management functions in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,40 +1,40 @@
-// const category = require("../models/categorySchema")
+const category = require("../models/categorySchema")
 
-// let userController  = ()=>{
-//     console.log("hello")
-// }
+let userController  = ()=>{
+    console.log("hello")
+}
 
-// let createCategory = async(req,res)=>{
-//     let{name}=req.body
-//     let existingName = await category.findOne({name:name.toLowerCase()})
-//     if(existingName){
-//     return res.status(400).json({
-//             success:false,
-//             message:"Category already existis"
-//         }) 
+let createCategory = async(req,res)=>{
+    let{name}=req.body
+    let existingName = await category.findOne({name:name.toLowerCase()})
+    if(existingName){
+    return res.status(400).json({
+            success:false,
+            message:"Category already existis"
+        }) 
 
-//     }
-//     let category =new category({
-//         name:name.toLowerCase()
-//     })
-//     category.save()
-//   return res.status(201).json({
-//             success:true,
-//             message:"Category created"
-//         }) 
+    }
+    let category =new category({
+        name:name.toLowerCase()
+    })
+    category.save()
+  return res.status(201).json({
+            success:true,
+            message:"Category created"
+        }) 
    
 
-// }
-// let getAllCategory = async(req,res)=>{
-//     let category = await category.find({})
+}
+let getAllCategory = async(req,res)=>{
+    let category = await category.find({})
 
-//     res.status(200).json({
-//         success:true,
-//         message:"All Category",
-//         data: category
-//     })}
+    res.status(200).json({
+        success:true,
+        message:"All Category",
+        data: category
+    })}
 
-// module.exports = {userController,createCategory,getAllCategory}
+module.exports = {userController,createCategory,getAllCategory}
 
 
 
